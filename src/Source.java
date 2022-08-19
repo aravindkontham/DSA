c66be3e5a0f0a2de0c07e9930c52376e2d6b62b4
aravindkontham <lallithadevender11@gmail.com>
2022-08-19 23:43:36 +0530

    power with recursion

--- src/Source.java
+++ src/Source.java
@@ -1,15 +1,52 @@
 import java.util.Scanner;
-class Source{
+
+public class Source {
+    // function to calculate base^exponent
+    public static double powerfunc(double base, double power) {
+        //write your code here
+        double res=1;
+       if(power==0)
+           return 1;
+       else if(power>0) {
+           for (int i = 0; i < power; i++) {
+               res = res * base;
+           }
+           return res;
+       }
+       else if(power<0) {
+          // System.out.println(String.valueOf(power).substring(1));
+           Double tem=Double.parseDouble(String.valueOf(power).substring(1));
+          // System.out.println(tem);
+           for (int i = 0; i < tem; i++) {
+               res = (res * base);
+           }
+           return (1/res);
+           //do {
+//               res = res * base;
+//               power++;
+//           }
+//           while (power!=  0) ;
+//
+//
+//
+//
+//           return 1/res;
+       }
+           else{
+            System.out.println("Enter a valid power:");
+
+        }
+       return -1;
+    }
     public static void main(String[] args) {
-        Scanner scan =new Scanner(System.in);
-        System.out.println("Enter the natural number n:");
-        int n =scan.nextInt();
-        System.out.println("Sum of consequtive numbers is:"+addnumbers(n));
+        Scanner inpt = new Scanner(System.in);
+        System.out.println("Enter the base:");
+        double a = inpt.nextDouble();
+        System.out.println("Enter the power");
+        double b = inpt.nextDouble();
+        // printing the value of a^b i.e.,
+
+        System.out.print( powerfunc(a, b));
     }
-    public static int addnumbers(int num){
-        if(num!=0)
-            return num+addnumbers(num-1);
-        else
-            return num;
-    }
+
 }
